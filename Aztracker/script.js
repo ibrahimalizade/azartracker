b43b5abd659ffa70ade4ec6614542020d56f2e96
Ibrahim <80769933+ibrahimalizade@users.noreply.github.com>
2022-12-04 11:03:53 +0400

    Delete Aztracker directory

--- Aztracker/script.js
+++
@@ -1,6 +0,0 @@
-let page = document.querySelector(".body");
-let themeButton = document.querySelector(".themeButton");
-themeButton.onclick = function() {
-    page.classList.toggle("lightTheme");
-    page.classList.toggle("darkTheme");
-    };
